24d3d0ec6e3933a470d208080af90aae1b8991cc
jupiterb <bialyp26@gmail.com>
2024-05-16 14:26:59 +0200

    new data model

--- easynotes/notes.py
+++ easynotes/notes.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import json
 import uuid
 
@@ -19,10 +21,21 @@ class NoteData(BaseModel):
     articles: list[Article] = Field(default_factory=list)
 
 
+class NoteNode(BaseModel):
+    data: NoteData
+    edges: list[str] = Field(default_factory=list)
+
+
 class Tree(BaseModel):
     root: str
-    notes: dict[str, NoteData] = Field(default_factory=dict)
-    edges: dict[str, list[str]] = Field(default_factory=dict)
+    notes: dict[str, NoteNode] = Field(default_factory=dict)
+
+    @staticmethod
+    def init() -> Tree:
+        root_id = _new_id()
+        root_data = NoteData(title="Start")
+        root_node = NoteNode(data=root_data)
+        return Tree(root=root_id, notes={root_id: root_node})
 
 
 @dataclass
@@ -41,35 +54,31 @@ class NotesRepository:
         return self[self._tree.root]
 
     def destinations(self, source_id: str) -> list[Note]:
-        return [self[id] for id in self._tree.edges[source_id]]
+        return [self[id] for id in self._tree.notes[source_id].edges]
 
     def source(self, id: str) -> Note | None:
-        search = [note for note in self if id in self._tree.edges[note.id]]
+        search = [note for note in self if id in self._tree.notes[note.id].edges]
         return search[0] if any(search) else None
 
     def get(self, id: str) -> Note:
-        return Note(id, self._tree.notes[id])
+        return Note(id, self._tree.notes[id].data)
 
     def add(self, data: NoteData, source_id: str) -> Note:
         id = _new_id()
-
-        self._tree.notes[id] = data
-        self._tree.edges[id] = []
-        self._tree.edges[source_id].append(id)
-
+        self._tree.notes[id] = NoteNode(data=data)
+        self._tree.notes[source_id].edges.append(id)
         return self[id]
 
     def remove(self, id: str) -> None:
         for note in self.destinations(id):
             self.remove(note.id)
 
-        source = self.source(id=id)
+        source = self.source(id)
 
         if source is not None:
-            self._tree.edges[source.id].remove(id)
+            self._tree.notes[source.id].edges.remove(id)
 
         del self._tree.notes[id]
-        del self._tree.edges[id]
 
     def load(self) -> None:
         try:
@@ -77,17 +86,12 @@ class NotesRepository:
                 notes_data_json = json.load(file)
                 self._tree = Tree(**notes_data_json)
         except FileNotFoundError:
-            self._init_tree()
+            self._tree = Tree.init()
             self.save()
 
     def save(self) -> None:
         with open(self._file_path, "w") as file:
             json.dump(self._tree, file, default=lambda obj: obj.__dict__)
-
-    def _init_tree(self) -> None:
-        root_id = _new_id()
-        root_data = NoteData(title="Start")
-        self._tree = Tree(root=root_id, notes={root_id: root_data}, edges={root_id: []})
 
     def __len__(self) -> int:
         return len(self._tree.notes)
